089d33eaf8455627ecd5de22a33e0747d3ed8125
Haruka08 <haruka.hashimoto08@gmail.com>
2022-09-19 09:29:09 +0100

    fir st commit after trying to resolve ssh key issue

---
+++ Develop/server.js
@@ -0,0 +1,85 @@
+// importing express
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3001;
+
+const notes = require('./db/db');
+const uuid = require('./helpers/uuid');
+
+// initialising express()
+const app = express();
+
+// to convert client side data to be parsed to JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+  });
+
+// read db.json
+app.get('/api/notes', (req, res) => {
+    console.info(req.body);
+    const notes = req.body;
+    return res.json(notes);
+    });
+
+// write the new note to db.json
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to get reviews`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuid(),
+      };
+  
+      // Obtain existing reviews
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNote = JSON.parse(data);
+  
+          // Add a new review
+          parsedNote.push(newNote);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNote, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated reviews!')
+          );
+        }
+    })
+
+    const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+      const htmlPageContent = updateHTML(response);
+
+    } else {
+      res.status(500).json('Error in posting review');
+    }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
